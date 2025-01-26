8ef0af1e2d3e9e004daea12cadfaef3dfe163870
R-a-bo <birchgeorge8@gmail.com>
2025-01-26 13:31:22 -0600

    Initial commit; create project

---
+++ BirchFetchTakeHome/BirchFetchTakeHome/BirchFetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirchFetchTakeHomeApp.swift
+//  BirchFetchTakeHome
+//
+//  Created by George Birch on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BirchFetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirchFetchTakeHome/BirchFetchTakeHomeTests/BirchFetchTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirchFetchTakeHomeTests.swift
+//  BirchFetchTakeHomeTests
+//
+//  Created by George Birch on 1/26/25.
+//
+
+import Testing
+@testable import BirchFetchTakeHome
+
+struct BirchFetchTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
