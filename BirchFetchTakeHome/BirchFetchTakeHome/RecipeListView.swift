16e0197d3747cdc6a5476ed16cdf16c1e8908d62
R-a-bo <birchgeorge8@gmail.com>
2025-01-26 13:39:23 -0600

    Setup project structure

---
+++ BirchFetchTakeHome/BirchFetchTakeHome/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  BirchFetchTakeHome
+//
+//  Created by George Birch on 1/26/25.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
---
+++ BirchFetchTakeHome/BirchFetchTakeHome/RecipeListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeListViewModel.swift
+//  BirchFetchTakeHome
+//
+//  Created by George Birch on 1/26/25.
+//
+
+import Foundation
+
+class RecipeListViewModel {
+    
+}
